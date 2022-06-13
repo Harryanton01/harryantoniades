3fcdfb12cc5752261dfed1b5dd77644d3c7f00ef
Harry Antoniades <harryantoniades1@gmail.com>
2022-06-13 14:30:39 +0100

    updated site title and icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/compat/app";
+
+// Use your config values here.
+firebase.initializeApp({
+  apiKey: "AIzaSyAY6bgGlaTg2VyTHPtMAiuwQbHVozPKKiI",
+  authDomain: "harryantoniades-240c5.firebaseapp.com",
+  projectId: "harryantoniades-240c5",
+  storageBucket: "harryantoniades-240c5.appspot.com",
+  messagingSenderId: "846195080212",
+  appId: "1:846195080212:web:08188510b225945b42bb01",
+  measurementId: "G-DTHSEEXB6K",
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
